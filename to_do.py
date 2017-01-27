79fd69142a9a0d332076bb557c2aa4b1259999d1
Joseph Mushyakov <jmush32456@gmail.com>
2017-01-26 19:59:25 -0500

    save

--- to_do.py
+++ to_do.py
@@ -3,6 +3,7 @@
 def clear_file(file_name):
     with open(file_name, "w") as f:
         f.write("")
+        f.close()
 
 def add_item(file_name):
     add_item = input("Is there anything that you want to add? Type `clear` to clear your list. > ")
@@ -11,18 +12,24 @@ def add_item(file_name):
     else:
         with open(file_name, "a") as f:
             f.write(add_item + "\n")
+            f.close()
 
 def remove_item(file_name):
     remove_item = input("Is there anything that you would like to delete? Type `clear` to clear your list. > ")
     if remove_item == "clear":
         clear_file(file_name)
     else:
+        with open(file_name,"r") as f:
+            read = f.read()
+            f.close()
         with open(file_name,"w") as f:
-            for line in f.read():
-                if remove_item not in line:
-                        f.write(line)
+            for line in read:
+                if line != remove_item:
+                    f.write(line)
+            f.close()
 
 def read(file_name):
     with open(file_name,"r") as f:
         read = f.read()
         print(read)
+        f.close()
